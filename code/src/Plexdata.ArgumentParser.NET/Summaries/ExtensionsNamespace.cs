2e9781f08ddf221d656d2eb76b9f1c6355595a1d
Axel Kesseler <axel.kesseler@gmx.net>
2019-07-28 11:44:28 +0200

    Adding missing branch files.

---
+++ code/src/Plexdata.ArgumentParser.NET/Summaries/ExtensionsNamespace.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2019 plexdata.de
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace Plexdata.ArgumentParser.Extensions
+{
+    /// <summary>
+    /// The <see cref="Plexdata.ArgumentParser.Extensions"/> namespace contains 
+    /// all supported class extensions of the <i>Plexdata Argument Parser</i>.
+    /// </summary>
+    /// <remarks>
+    /// Extensions usually implement additional functionality that may not belong 
+    /// to a particular class. This namespace contains all currently available 
+    /// implementations of this kind of functionality.
+    /// </remarks>
+    [System.Runtime.CompilerServices.CompilerGenerated]
+    class NamespaceDoc { }
+}
